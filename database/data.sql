acfde6fef393ccf7790d7c2d2c07cff7445f31a2
Mads Hougesen <madshougesen@gmail.com>
2021-11-25 22:33:38 +0100

    feat(database): dummy data

---
+++ database/data.sql
@@ -0,0 +1,12 @@
+/* Locations */
+INSERT INTO Locations (location_name)
+VALUES ('bedroom');
+/* MeasurementTypes */
+INSERT INTO MeasurementTypes(measurement_type_name)
+VALUES ('temperature');
+INSERT INTO MeasurementTypes(measurement_type_name)
+VALUES ('pressure');
+INSERT INTO MeasurementTypes(measurement_type_name)
+VALUES ('humidity');
+INSERT INTO MeasurementTypes(measurement_type_name)
+VALUES ('gas_resistance');
