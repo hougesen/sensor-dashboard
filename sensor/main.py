8c6c77dc2729a96a75539f1699521b23b15ca936
Mads Hougesen <madshougesen@gmail.com>
2021-11-25 18:21:19 +0100

    fix(sensor): turned timestamp into iso string

--- sensor/main.py
+++ sensor/main.py
@@ -1,11 +1,10 @@
-import requests
-import bme680
 import time
-import datetime
-
+import bme680
+import requests
+from datetime import datetime
 
 # API HOST
-HOST = "192.168.86.64:5000"
+API_HOST = "192.168.86.64:5000"
 
 # Location of pi
 LOCATION_ID = 1
@@ -25,6 +24,9 @@ sensor.set_pressure_oversample(bme680.OS_4X)
 sensor.set_temperature_oversample(bme680.OS_8X)
 sensor.set_filter(bme680.FILTER_SIZE_3)
 sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+sensor.set_gas_heater_temperature(320)
+sensor.set_gas_heater_duration(150)
+sensor.select_gas_heater_profile(0)
 
 
 def read_sensor_data():
@@ -45,17 +47,21 @@ def read_sensor_data():
         if sensor.data.heat_stable and sensor.data.gas_resistance:
             print(f"gas_resistance: {sensor.data.gas_resistance}")
             send_sensor_data(sensor.data.gas_resistance, 4)
+        else:
+            print("sensor.data.heat_stable is not stable")
 
 
 def send_sensor_data(value, type):
-    URL = "/measurement/new"
     try:
-        requests.post(f"{HOST}/{URL}", data={
+        sensor_data = {
             "measurement_type_id": type,
             "location_id": LOCATION_ID,
             "measurement_value": value,
-            "measurement_time": datetime.datetime.now()
-        })
+            "measurement_time": datetime.now().isoformat()
+        }
+
+        r = requests.post(f"{API_HOST}/measurement/new", json=sensor_data)
+        print("r", r)
 
     except requests.exceptions.RequestException as error:
         print("Something went wrong sending sensor data", error)
@@ -63,9 +69,10 @@ def send_sensor_data(value, type):
 
 try:
     while True:
-        print(f"Starting new cycle {datetime.datetime.now()}")
+        print(f"Starting new cycle {datetime.now()}")
         read_sensor_data()
 
+        # Sleep
         time.sleep(POLLING_FREQ)
 
 except KeyboardInterrupt:
